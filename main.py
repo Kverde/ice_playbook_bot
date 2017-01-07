ec36304b91b1507ef267cb0fd2fa3800bebf64c7
Kverde <Kverde@yandex.ru>
2017-01-07 17:33:03 +0500

    add menu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 import telegram
 
 from source.setting import Setting
@@ -11,9 +10,17 @@ APP_ID = 'IcePlayBookBot'
 setting = Setting(APP_ID)
 itemLoader = InfoItemLoader(setting)
 
-def start(bot, update):
-    update.message.reply_text('В ответ на любое сообщение вы получите случайную запись!')
-    update.message.reply_text(r'Список источников вы может получить командой /about')
+BUTTON_PREV = 0
+BUTTON_NEXT = 1
+BUTTON_RANDOM = 2
+BUTTON_ABOUT = 3
+
+BUTTON_CAPTIONS = {
+    BUTTON_PREV: 'Предыдущая запись',
+    BUTTON_NEXT: 'Следующая запись',
+    BUTTON_RANDOM: 'Случайная запись',
+    BUTTON_ABOUT: 'Спиок источников',
+}
 
 about_text = '''
   Записи берутся из разных источников, список:
@@ -23,12 +30,12 @@ about_text = '''
   Если здесь не упомянут какой-то из источников записей, пожалуйста, свяжитесь с @Kverde
 '''
 
-def about(bot, update):
+def sendAbout(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
                     text=about_text,
                     parse_mode=telegram.ParseMode.HTML)
 
-def text(bot, update):
+def sendRandomItem(bot, update):
     try:
         item = itemLoader.loadRandomItem()
 
@@ -41,11 +48,81 @@ def text(bot, update):
     except Exception as e:
         print(e)
 
+def sendMenu(bot, update):
+    keyboard = [[KeyboardButton(BUTTON_CAPTIONS[BUTTON_PREV], callback_data=BUTTON_PREV),
+                 KeyboardButton(BUTTON_CAPTIONS[BUTTON_NEXT], callback_data=BUTTON_NEXT)],
+
+                [KeyboardButton(BUTTON_CAPTIONS[BUTTON_RANDOM], callback_data=BUTTON_RANDOM)],
+                [KeyboardButton(BUTTON_CAPTIONS[BUTTON_ABOUT], callback_data=BUTTON_ABOUT)]]
+
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Для навигации используйте меню:', reply_markup=reply_markup)
+
+def sendHideMenu(bot, update):
+    reply_markup = telegram.ReplyKeyboardHide()
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text="Скрыть клавиатуру",
+                    reply_markup=reply_markup)
+
+def sendNextItem(bot, update):
+    pass
+
+def sendPrevItem(bot, update):
+    pass
+
+
+def cm_start(bot, update):
+    update.message.reply_text('Для навигации используйте меню или команды.')
+
+    sendMenu(bot, update)
+
+MENU_EVENTS = {
+    BUTTON_CAPTIONS[BUTTON_PREV]: sendNextItem,
+    BUTTON_CAPTIONS[BUTTON_NEXT]: sendPrevItem,
+    BUTTON_CAPTIONS[BUTTON_RANDOM]: sendRandomItem,
+    BUTTON_CAPTIONS[BUTTON_ABOUT]: sendAbout,
+}
+
+def callb_error(bot, update, error):
+    print('Update "%s" caused error "%s"' % (update, error))
+
+def cm_about(bot, update):
+    sendAbout(bot, update)
+
+def cm_next(bot, update):
+    sendNextItem(bot, update)
+
+def cm_prev(bot, update):
+    sendPrevItem(bot, update)
+
+def cm_random(bot, update):
+    sendRandomItem(bot, update)
+
+def cm_showMenu(bot, update):
+    sendMenu(bot, update)
+
+def cm_hideMenu(bot, update):
+    sendHideMenu(bot, update)
+
+def callb_text(bot, update):
+    if update.message.text in MENU_EVENTS:
+        MENU_EVENTS[update.message.text](bot, update)
+    else:
+        sendRandomItem(bot, update)
+
 updater = Updater(setting.telegram_token)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('about', about))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, text))
+updater.dispatcher.add_handler(CommandHandler('start', cm_start))
+updater.dispatcher.add_handler(CommandHandler('about', cm_about))
+updater.dispatcher.add_handler(CommandHandler('next', cm_next))
+updater.dispatcher.add_handler(CommandHandler('prev', cm_prev))
+updater.dispatcher.add_handler(CommandHandler('showmenu', cm_showMenu))
+updater.dispatcher.add_handler(CommandHandler('hidemenu', cm_hideMenu))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, callb_text))
+
+updater.dispatcher.add_error_handler(callb_error)
 
 updater.start_polling()
 updater.idle()
